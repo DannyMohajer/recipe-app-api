7ed4910ff959a5bfc481438afd241c2b9e35cf03
Daniel Mohajer <daniel.mohajer.mohacel@gmail.com>
2025-02-01 16:42:46 +0330

    Add user profile management functionality and update user serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -23,9 +23,21 @@ class UserSerializer(serializers.ModelSerializer):
         """Creata and return a user with encripted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
+
     email = serializers.EmailField()
     password = serializers.CharField(
         style={"input_type": "password"},
@@ -43,7 +55,7 @@ class AuthTokenSerializer(serializers.Serializer):
         )
         if not user:
             msg = _("Unable to authenticate with provided cerdtials.")
-            raise serializers.ValidationError(msg,code='authorization')
+            raise serializers.ValidationError(msg, code="authorization")
 
-        attrs['user'] = user
+        attrs["user"] = user
         return attrs
