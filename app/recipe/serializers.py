a71907d9481c5918e15ccfcf00e0c1416eb2e704
Daniel Mohajer <daniel.mohajer.mohacel@gmail.com>
2025-02-03 12:05:07 +0330

    Add description field to RecipeSerializer and enhance recipe API tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,20 +1,23 @@
 """
 Serializers for reciper APIs
 """
+
 from rest_framework import serializers
 
 from core.models import Recipe
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for Recipes."""
 
     class Meta:
         model = Recipe
-        fields = ['id','title','time_minutes','price','link']
-        ready_only_fields = ['id']
+        fields = ["id", "title", "time_minutes", "price", "link", "description"]
+        ready_only_fields = ["id"]
+
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ["description"]
