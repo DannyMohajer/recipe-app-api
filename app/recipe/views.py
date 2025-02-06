a414dff03131dd2a28773cc180094d81c7e1a8aa
Daniel Mohajer <daniel.mohajer.mohacel@gmail.com>
2025-02-06 10:25:46 +0330

    Add delete functionality to Tag API and enhance TagViewSet

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -42,8 +42,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet,
+):
     """Manage tags in the databse."""
+
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_class = [TokenAuthentication]
